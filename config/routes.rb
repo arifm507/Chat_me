ed4451b3f7874c75bf6d4e3fb993ceb4886ed683
Arif <Arifm507@gmail.com>
2019-09-14 17:29:58 +0530

    Final App

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  post 'message', to: 'messages#create'
+
+
+  mount ActionCable.server, at: '/cable'
 
 end
